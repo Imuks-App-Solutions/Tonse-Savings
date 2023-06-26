4ee0040e4de77738df1cf301992d36f5ce07516c
Earnest Muyunda <emuyundajnr@gmail.com>
2023-06-26 14:29:30 +0100

    created Routing for signup page & Dasboard page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
-import SignUp from "./Components/SignUp"
+import Navbar from './Components/Navbar'
+import Dashboard from './Pages/Dashboard'
+import SignUp from "./Pages/SignUp"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+     <BrowserRouter>
+      <Routes>
+    <Route index element= {<SignUp/>} />
+    <Route path = "/Signup" element= {<SignUp/>} />
+    <Route path = "/Dashboard" element= {<Dashboard/>} />
+    </Routes>  
+    </BrowserRouter>
       <SignUp/>
-    </>
+      <Navbar/>
+      <Dashboard/>
+      </>
   )
 }
 
