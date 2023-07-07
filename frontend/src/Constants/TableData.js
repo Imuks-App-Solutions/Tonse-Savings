f1a9e1244c2bb6aadcf75db953a6f5c0957dbb3d
DavidMtonga <138039993+DavidMtonga@users.noreply.github.com>
2023-07-07 12:05:00 +0200

    Add files via upload

---
+++ frontend/src/Constants/TableData.js
@@ -0,0 +1,40 @@
+export const TableData = [
+  {
+    id: Math.random().toString(4),
+    invoiceName: "Invoice #007-Dec 2022",
+    balance: "paid",
+    billDate: "Dec 1, 2022",
+    amount: "10.00",
+    plan: "Basic",
+    users: "10",
+  },
+  {
+    id: Math.random().toString(4),
+    invoiceName: "Invoice #006-Dec 2022",
+    balance: "unpaid",
+    billDate: "Nov 1, 2022",
+    amount: "10.00",
+    plan: "Basic",
+    users: "9",
+  },
+  {
+    id: Math.random().toString(4),
+    invoiceName: "Invoice #005-Dec 2022",
+    balance: "paid",
+    billDate: "Oct 1, 2022",
+    amount: "10.00",
+    plan: "Basic",
+    users: "8",
+  },
+  {
+    id: Math.random().toString(4),
+    invoiceName: "Invoice #004-Dec 2022",
+    balance: "paid",
+    billDate: "Sep 1, 2022",
+    amount: "10.00",
+    plan: "Basic",
+    users: "8",
+  },
+
+];
+  
