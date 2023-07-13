b3024209cafa4b11dc18bb61cca3ea25adfae622
GloriaS101 <137299287+GloriaS101@users.noreply.github.com>
2023-07-13 12:13:30 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import SignUp from "./Pages/SignUp";
 import Dashboard from "./Pages/Dashboard";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
+import Card from "./Components/Card";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/Signup" element={<SignUp />} />
           <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/Home" element={<Home />} />
+          <Route path="/Card" element={<Card />} />
         </Routes>
       </BrowserRouter>
     </>
