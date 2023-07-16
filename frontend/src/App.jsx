18106e55e4c5c2db61e0a816e6ac76a5318780fc
emuyundajnr26 <emuyundajnr@gmail.com>
2023-07-16 21:31:13 +0300

    linked penges page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import SignUp from "./Pages/SignUp";
 import Dashboard from "./Pages/Dashboard";
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
+import Messageb from "./Pages/Messageb";
 import Card from "./Components/Card";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./shared/authContext";
@@ -18,6 +19,7 @@ function App() {
             <Route path="/Dashboard" element={<Dashboard />} />
             <Route path="/Home" element={<Home />} />
             <Route path="/Card" element={<Card />} />
+            <Route path="/Messageb" element={<Messageb />} />
           </Routes>
         </BrowserRouter>
       </AuthContextProvider>
---
+++ frontend/src/Components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+        Add whatever you like here!
+    </div>
+  )
+}
+
+export default Hero
